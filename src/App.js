bed2a6dc1e02afb0ffcc4b50dbe30180bae8685b
skck0226 <skck0226@ajou.ac.kr>
2020-12-09 04:29:27 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import react, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [list,setList] = useState([]);
+	const handleChange = ()=>{
+		
+	}
+	const submit = ()=>{
+		setList({...list},)
+	}
+	return(
+		<div>
+			<input type='text' onChange={handleChange} />
+			<button onClick={submit}>submit</button>
+		</div>
+	);	
 }
 
 export default App;
